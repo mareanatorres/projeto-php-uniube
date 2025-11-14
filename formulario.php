2aa460a77340ac90d67ba5242fcabcd2b941a90d
Mariana Torres <marisilva_torres@hotmail.com>
2025-11-13 21:00:42 -0300

    aula-1

---
+++ formulario.php
@@ -0,0 +1,14 @@
+<html>
+
+    <body>
+
+        <form action="http://localhost/projeto-php/receberFormulario.php" method="post"><br>
+            <label for="nome">Nome:</label><input type="text" name="nome" id="nome"><br>
+            <label for="telefone">Telefone:</label><input type="text" name="telefone" id="telefone"><br>
+            <label for="email">Email:</label><input type="text" name="email" id="email"><br>
+            <button type="submit">Enviar dados</button>
+        </form>
+
+    </body>
+
+</html>
